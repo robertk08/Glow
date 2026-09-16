9b367b4e29a9fad306761d74dc5cee76252eac68
Robert Krause <robertkrause@gmx.at>
2026-09-16 12:16:48 +0200

    Initial Commit

---
+++ Glow/GlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlowApp.swift
+//  Glow
+//
+//  Created by Robert Krause on 16.09.26.
+//
+
+import SwiftUI
+
+@main
+struct GlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
